453ffa43f1f9d4b2c249b1c4818dab2959e40de9
colowokanga <colowokanga@gmail.com>
2022-08-23 20:34:20 -0700

    0x01

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for alpha in reversed(range(97, 123)):
+    if (alpha % 2 == 0):
+        print("{:c}".format(alpha), end='')
+    else:
+        print("{:c}".format(alpha - 32), end='')
+        
